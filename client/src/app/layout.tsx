55b827b58b1ad1005db9081e4c862e95b5c8b00d
hoangsangho <hoangsangho93@gmail.com>
2025-07-02 22:17:13 +0700

    config font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Roboto } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const roboto = localFont({
+  src: [
+    {
+      path: "../font/Roboto-Bold.ttf",
+      weight: "700",
+      style: "normal"
+    },
+    {
+      path: "../font/Roboto-Regular.ttf",
+      weight: "400",
+      style: "normal"
+    },
+  ],
+  variable: '--font-roboto'
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const roboto = Roboto({
+//   subsets: ["vietnamese"],
+//   weight: ["100", "200"]
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +35,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${roboto.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
